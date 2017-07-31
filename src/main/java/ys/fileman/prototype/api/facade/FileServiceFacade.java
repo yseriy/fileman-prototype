b9afd890aa2cd24cbed91300d74ee446c4de4e58
yseriy <goodyar@yandex.ru>
2017-07-31 18:14:08 +0300

    Новый сервис и тест к нему.

--- src/main/java/ys/fileman/prototype/api/facade/FileServiceFacade.java
+++ src/main/java/ys/fileman/prototype/api/facade/FileServiceFacade.java
@@ -8,28 +8,29 @@ import ys.fileman.prototype.domen.Credentials;
 import ys.fileman.prototype.domen.File;
 import ys.fileman.prototype.domen.Path;
 import ys.fileman.prototype.domen.Transport;
-import ys.fileman.prototype.service.AuthService;
+import ys.fileman.prototype.service.AuthenticationService;
 import ys.fileman.prototype.service.TransportService;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 import java.util.List;
 
 @Service
 public class FileServiceFacade {
 
-    private final AuthService authService;
+    private final AuthenticationService authenticationService;
     private final TransportService transportService;
     private final ResponseAssembler responseAssembler;
 
-    public FileServiceFacade(AuthService authService, TransportService transportService, ResponseAssembler responseAssembler) {
-        this.authService = authService;
+    public FileServiceFacade(AuthenticationService authenticationService, TransportService transportService, ResponseAssembler responseAssembler) {
+        this.authenticationService = authenticationService;
         this.transportService = transportService;
         this.responseAssembler = responseAssembler;
     }
 
     public ResponseDTO list(HttpServletRequest httpServletRequest, String brand, String contract, String account,
-                            String token, PathDTO pathDTO) {
-        Credentials credentials = authService.getCredentials(brand, contract, account, token);
+                            String token, PathDTO pathDTO) throws IOException {
+        Credentials credentials = authenticationService.getCredentials(brand, contract, account, token);
         Transport transport = transportService.getFTPTransport(credentials);
         List<File> fileList = transport.list(new Path(pathDTO.getPath()));
         ResponseDTO<List<File>> responseDTO = responseAssembler.getSuccessResponse(httpServletRequest);
