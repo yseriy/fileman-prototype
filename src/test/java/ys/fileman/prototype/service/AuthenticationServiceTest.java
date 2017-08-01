d4825d118c7065e1e4d5c3670283f8e16a20ebbd
yseriy <goodyar@yandex.ru>
2017-08-01 16:36:35 +0300

    Очистка кода.

--- src/test/java/ys/fileman/prototype/service/AuthenticationServiceTest.java
+++ src/test/java/ys/fileman/prototype/service/AuthenticationServiceTest.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
